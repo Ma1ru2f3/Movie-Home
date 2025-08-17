f656561064ce33a9ae6d8f3ca53bf6ff0fd0fd83
Ma1ru2f3 <Mymaruf94@gmail.com>
2025-08-17 23:54:14 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Category Filter
-const categoryBtns = document.querySelectorAll('.category-btn');
-const movieCards = document.querySelectorAll('.movie-card');
-
-categoryBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    categoryBtns.forEach(b => b.classList.remove('active'));
-    btn.classList.add('
