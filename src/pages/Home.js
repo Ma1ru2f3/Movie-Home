d7eaf6311ebc5a4f8f61135ce50e29efda3e270b
Ma1ru2f3 <Mymaruf94@gmail.com>
2025-08-19 23:15:32 +0600

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ padding: "20px", marginBottom: "60px" }}>
+      <h2>Welcome to Movie World!</h2>
+      <p>Here will be the video list (coming soon).</p>
+    </div>
+  );
+};
+
+export default Home;
